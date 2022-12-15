6c4953e20ecb1a7968421cc6f6c89b74b0ac4c43
Ryan Gole <ryan.m.gole@gmail.com>
2022-12-15 18:17:21 -0500

    Parsing for part one fixed, need to clean it up

--- src/Week2/Day13/DistressSignal.ts
+++ src/Week2/Day13/DistressSignal.ts
@@ -25,7 +25,63 @@ let test: string =
 [1,[2,[3,[4,[5,6,7]]]],8,9]
 [1,[2,[3,[4,[5,6,0]]]],8,9]`;
 
+let test2: string = 
+`[[],1]
+[[],2]
+
+[[],2]
+[[],1]
+
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+
+[[[[],[10,0,1],4]],[[0],7],[0,[],[]],[],[]]
+[[7,[[],[1],[5,5,6],[10,3],5],10,10],[[]],[5,[2,2],1,4]]
+
+[[10,[10,[9,8,3],3,0],[[3,10,10],[5,8,5]],[],7],[],[8],[7,1]]
+[[7,1],[[2],[[3,1,7,8,3],7]]]
+
+[[[5,[9,4,7,6],5,[8],[7,3]],[],[4],[[3,8],[8,8,9,6,9],[1,5,1]]],[[],5]]
+[[10,[8,[9,6],4,[6,0,3]]],[]]
+
+[[5],[6,2,[9,[1,3,9]]],[[8,[2,0,8,2],[],[8,6,2,9]],9,5],[3,0,3,8]]
+[[],[[],[],[],[6,8,[5,10,6],7],0],[[[0,5,7],[],[0,3,4,10,2],[1,0,1],[10]],8,[1,4]],[[[10,2,10,10,2],3,6,8,0],2,5]]
+
+[[5,3,6,9],[[7,9],8,[[1,8],0,[1],8,[]],10,6],[1],[2,[[7,2,5],[2,7]],[[],10,[],[5,10,1],10],[6,[1,1],[7,6,10,2,1],0],[]],[[8,[0],0],5,4,[],1]]
+[[[5,[0,8,0,9,0],[1,8,9],[1,9]],[8,4,[],[3,10,4],3],1,9,[]],[[1,10,2],6],[5,5,10],[[1,[0,5,0],[2,6,1],4,7],[[1]],[10,[1,0],2,[],4],[[8,8,7,8],[2,6]]]]
+
+[[2,8]]
+[[[[5],[9,9,6],[1,8],[5,4,6,0,2]],[[5,9],[]],[7,[4,2,3,4],6]],[],[1,9,7,[6]]]
+
+[[5,[7]],[[6,3],[8,[]],6],[[[3,8,1,1,3]],1,7,[],0],[1,[],[],0],[]]
+[]
+
+[1,2,3,[1,2,3],4,1]
+[1,2,3,[1,2,3],4,0]`
+
 console.log(partOne(test));
+console.log(partOne(test2));
 console.log(partTwo(test));
 
 fs.readFile('./src/Week2/Day13/input.txt', 'utf8', (err: any, data: any) => {
@@ -49,10 +105,17 @@ function partOne(text: string): number {
     let lines: string[] = text.includes('\r\n') ? text.split('\r\n\r\n') :text.split('\n\n');
     let pairs: string[][] = lines.map(x => x.split(text.includes('\r\n') ? '\r\n' : '\n'));
     let sum: number = 0;
+    let inOrder: string[][] = [];
+    let outOfOrder: string[][] = [];
     for (let i = 0; i < pairs.length; i++) {
         let result = compare(parseString(pairs[i][0]), parseString(pairs[i][1]));
-        if (result || result === null)
+        if (result || result === null) {
             sum += i + 1;
+            inOrder.push(pairs[i]);
+        }
+        else {
+            outOfOrder.push(pairs[i]);
+        }
     }
 
     return sum;
@@ -61,7 +124,9 @@ function partOne(text: string): number {
 function parseString(text: string): any[] {
     let stack: any[] = [];
     let values: any[] = [];
+    let commaStack: boolean[] = [];
     let curr = '';
+    let isPrecedingComma = false; // TODO: this needs to be tracked at the stack level I think, so a second array of bools at each level tracking this.
     for (let i = 1; i < text.length-1; i++) { // can exclude first and last []
         let char = text[i];
         if (char === ',') {
@@ -77,9 +142,31 @@ function parseString(text: string): any[] {
             stack.push([]);
         }
         else if (char === ']') { // closing.
-            stack[stack.length - 1].push(curr);
-            curr = '';
-            values.push(stack.pop());
+            if (curr != '') {
+                stack[stack.length - 1].push(curr);
+                curr = '';
+                values.push(stack.pop());
+            }
+            else {
+                // if (isPrecedingComma) {
+                //     if (stack.length > 0)
+                //         stack[stack.length - 1].push(curr);
+                //     else
+                //         values.push(curr);
+                // }
+                // else {
+
+                // }
+                if (stack.length > 0)
+                    stack[stack.length - 1].push(stack.pop());
+                else
+                    values.push(stack.pop());
+                // A comma signifies a new value to be added.
+                // If there are no commas... we should be inserting
+                // Seems easier if I just make this recursive so I don't need to track
+                // where commas are at each level.
+                // Nevermind, tried writing recursively and it requires comma tracking and seems a headache.
+            }
         }
         else {
             curr += char;
@@ -90,6 +177,57 @@ function parseString(text: string): any[] {
     }
     return values;
 }
+
+// function recursiveParse(text: string): any[] {
+//     let values: any = [];
+//     let curr = '';
+//     let isPrecedingComma = false;
+//     for (let i = 0; i < text.length; i++) { // can exclude first and last []
+//         let char = text[i];
+//         if (char === '[') {
+//             if (isPrecedingComma)
+//             return recursiveParse(text.substring(i+1));
+//         }
+//         else if (char === ']') {
+            
+//         }
+//         else if (char === ',') {
+//             values.push(curr);
+//             curr = '';
+//         }
+//         else {
+//             curr += char;
+//         }
+//         if (char === ',') {
+//             if (curr !== '') {
+//                 if (stack.length > 0)
+//                     stack[stack.length - 1].push(curr);
+//                 else
+//                     values.push(curr);
+//                 curr = '';
+//             }
+//         }
+//         else if (char === '[') { // opening.
+//             stack.push([]);
+//         }
+//         else if (char === ']') { // closing.
+//             if (curr != '') {
+//                 stack[stack.length - 1].push(curr);
+//                 curr = '';
+//                 values.push(stack.pop());
+//             }
+//             else {
+//                 // A comma signifies a new value to be added.
+//                 // If there are no commas... we should be inserting
+//                 // Seems easier if I just make this recursive so I don't need to track
+//                 // where commas are at each level.
+//             }
+//         }
+//         else {
+//             curr += char;
+//         }
+//     }
+// }
 
 function compare(left: any[], right: any[]): boolean | null {
     for (let i = 0; i < left.length; i++) {
@@ -123,7 +261,9 @@ function compare(left: any[], right: any[]): boolean | null {
             }
         }
     }
-    return null;
+    if (left.length === right.length)
+        return null;
+    return true;
 }
 
 function partTwo(text: string): number {
